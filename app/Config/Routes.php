d2ab33ad18215137a92f58af82835cebc45f2fd5
riyanboyuhu <riyanboyuhu@gmail.com>
2024-08-30 17:27:15 +0800

    memperbaiki tulisan filter di routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -22,14 +22,14 @@ $routes->get('/tiket', 'Home::tiketDownload', ['filter' => 'role:user']);
 $routes->post('/uploadcicil', 'Home::uploadCicil', ['filter' => 'role:user']);
 $routes->post('/cicilbyid', 'Home::cicilId', ['filter' => 'role:user']);
 
-$routes->get('admin', 'Admin\HomeAdmin::index', ['filter' => 'role:administrator', 'operator', 'bendahara']);
-$routes->get('admin/peserta', 'Admin\HomeAdmin::daftarPeserta', ['filter' => 'role:administrator', 'operator', 'bendahara']);
-$routes->get('admin/peserta/(:num)', 'Admin\HomeAdmin::pesertabyID/$1', ['filter' => 'role:administrator', 'operator']);
-$routes->get('admin/validasi', 'Admin\HomeAdmin::validations', ['filter' => 'role:administrator', 'operator', 'bendahara']);
-$routes->get('admin/validasi/(:num)', 'Admin\HomeAdmin::validationbyId/$1', ['filter' => 'role:administrator', 'operator', 'bendahara']);
-$routes->post('admin/validated', 'Admin\HomeAdmin::validated', ['filter' => 'role:administrator', 'operator', 'bendahara']);
+$routes->get('admin', 'Admin\HomeAdmin::index', ['filter' => 'role:administrator, operator, bendahara']);
+$routes->get('admin/peserta', 'Admin\HomeAdmin::daftarPeserta', ['filter' => 'role:administrator, operator, bendahara']);
+$routes->get('admin/peserta/(:num)', 'Admin\HomeAdmin::pesertabyID/$1', ['filter' => 'role:administrator, operator, bendahara']);
+$routes->get('admin/validasi', 'Admin\HomeAdmin::validations', ['filter' => 'role:administrator, operator, bendahara']);
+$routes->get('admin/validasi/(:num)', 'Admin\HomeAdmin::validationbyId/$1', ['filter' => 'role:administrator, operator, bendahara']);
+$routes->post('admin/validated', 'Admin\HomeAdmin::validated', ['filter' => 'role:administrator, operator, bendahara']);
 $routes->post('admin/addDiscount', 'Admin\HomeAdmin::addDiscount', ['filter' => 'role:administrator']);
 
-$routes->get('admin/bendahara', 'Admin\HomeAdmin::bendahara', ['filter' => 'role:administrator', 'operator', 'bendahara']);
-$routes->post('admin/adddebit', 'Admin\HomeAdmin::addDebit', ['filter' => 'role:administrator', 'operator', 'bendahara']);
-$routes->post('admin/addkredit', 'Admin\HomeAdmin::addKredit', ['filter' => 'role:administrator', 'operator', 'bendahara']);
+$routes->get('admin/bendahara', 'Admin\HomeAdmin::bendahara', ['filter' => 'role:administrator, operator, bendahara']);
+$routes->post('admin/adddebit', 'Admin\HomeAdmin::addDebit', ['filter' => 'role:administrator, operator, bendahara']);
+$routes->post('admin/addkredit', 'Admin\HomeAdmin::addKredit', ['filter' => 'role:administrator, operator, bendahara']);
